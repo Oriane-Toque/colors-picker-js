03202600be3eb575106866522a774497aec72dd5
Oriane-Toque <oriane.toque@gmail.com>
2021-05-04 15:51:08 +0200

    fonction pour changer fond en fct colorBox: done

--- js/app.js
+++ js/app.js
@@ -12,6 +12,30 @@ let app = {
     for (index in app.colors) {
       app.generateColorBox(app.colors[index]);
     }
+    
+    // je récupère tous les boxColor qui ont été créé dynamiquement
+    let allBgSelector = document.querySelectorAll('.bg-color-selector');
+
+    // je leur met un écouteur d'évènement à chacun
+    for(let bgSelector of allBgSelector) {
+      bgSelector.addEventListener('click', app.handleChangeBackground);
+    }
+    
+  },
+
+  // TODO fonction pour modifier le fond en fonction du colorBox sélectionné
+  handleChangeBackground: function(evt) {
+
+    let bgSelector = evt.currentTarget;
+    console.log(bgSelector);
+
+    // pour chaque colorBox on va récupérer la valeur de leur attribut style
+    let valueBgSelector = bgSelector.getAttribute('style');
+    console.log(valueBgSelector);
+
+    // on cible notre body et on modifie son style
+    // on attribut à style la valeur de nos colorBox respectives
+    document.body.style = valueBgSelector;
   },
 
   generateColorBox: function(color) {
